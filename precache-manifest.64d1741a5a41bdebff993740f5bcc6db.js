f0adb70fb6dbeb6c7085d9b1287954b2c9202de8
hanif <hnflasting@gmail.com>
2020-08-17 22:57:16 +0700

    Bismillah

---
+++ precache-manifest.64d1741a5a41bdebff993740f5bcc6db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d256548f434bcbaa85b35a9dad176b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312bc39f61c6668b6087",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "29d826d12915ff0994c1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "312bc39f61c6668b6087",
+    "url": "/static/js/2.1d92c178.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1d92c178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d826d12915ff0994c1",
+    "url": "/static/js/main.55f76475.chunk.js"
+  },
+  {
+    "revision": "6dcc06c1d1fae8bc25a1",
+    "url": "/static/js/runtime-main.79cdded9.js"
+  }
+]);
